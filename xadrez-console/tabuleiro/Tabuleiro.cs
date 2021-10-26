8ee58c07ce8a6fbbdd7e8dcd53cbab8ec012ccc7
caiomaia3 <caiomaia3@gmail.com>
2021-10-26 20:59:26 -0300

    Start the development of Tabuleiro.

---
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -0,0 +1,16 @@
+﻿namespace tabuleiro
+{
+    public class Tabuleiro
+    {
+        public int nLinhas { get; set; }
+        public int nColunas { get; set; }
+        private Peca[,] grid;
+
+        public Tabuleiro(int nLinhas, int nColunas)
+        {
+            this.nLinhas = nLinhas;
+            this.nColunas = nColunas;
+            this.grid = new Peca[nLinhas, nColunas];
+        }
+    }
+}
