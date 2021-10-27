cb3300e3c882ab7ec7d87425aed6c0d4b4fdc140
caiomaia3 <caiomaia3@gmail.com>
2021-10-26 21:39:11 -0300

    Starting the printing tabuleiro method.

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -9,6 +9,7 @@ namespace xadrez_console
         {
             Posicao p = new Posicao(3, 4);
             Tabuleiro tab = new Tabuleiro(8, 8);
+            Tela.imprimirTabuleiro(tab);
             Console.WriteLine("Posição: " + p);
             Console.ReadLine();
         }
--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -1,16 +1,22 @@
-﻿namespace tabuleiro
+﻿using tabuleiro;
+namespace tabuleiro
 {
-    public class Tabuleiro
+    class Tabuleiro
     {
         public int nLinhas { get; set; }
         public int nColunas { get; set; }
-        private Peca[,] grid;
+        private Peca[,] pecas;
 
         public Tabuleiro(int nLinhas, int nColunas)
         {
             this.nLinhas = nLinhas;
             this.nColunas = nColunas;
-            this.grid = new Peca[nLinhas, nColunas];
+            this.pecas = new Peca[nLinhas, nColunas];
+        }
+
+        public Peca peca(int linha, int coluna)
+        {
+            return pecas[linha, coluna];
         }
     }
 }
